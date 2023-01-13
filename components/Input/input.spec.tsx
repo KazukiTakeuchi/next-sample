3cef9ecc69b6bf7483906c56194d8a363051f796
“KazukiTakeuchi” <takeuchi1227813813@gmail.com>
2023-01-13 23:47:03 +0900

    文字を入力したら、入力した内容が表示される場合のテスト

--- components/Input/input.spec.tsx
+++ components/Input/input.spec.tsx
@@ -1,4 +1,4 @@
-import { render, screen, RenderResult,} from "@testing-library/react";
+import { render, screen, RenderResult, fireEvent,} from "@testing-library/react";
 import { Input } from "./index";
 
 describe("Input", () => {
@@ -18,4 +18,24 @@ describe("Input", () => {
 
     expect(inputNode).toHaveValue("");
   });
+
+  it("文字を入力したら、入力した内容が表示される", () => {
+    const inputText = "test"
+    const inputNode = screen.getByLabelText('Username') as HTMLInputElement;
+
+    fireEvent.change(inputNode, {target: {value: inputText}})
+
+    expect(inputNode).toHaveValue(inputText);
+  })
+
+  it("リセットボタンが押されたら入力されている内容がクリアされる", () => {
+    const inputText = "test";
+    const inputNode = screen.getByLabelText("Username") as HTMLInputElement;
+    fireEvent.change(inputNode, { target: { value: inputText } });
+
+    const buttonNode = screen.getByRole('button', { name: 'Reset' });
+    fireEvent.click(buttonNode);
+
+    expect(inputNode).toHaveValue('');
+  })
 });
